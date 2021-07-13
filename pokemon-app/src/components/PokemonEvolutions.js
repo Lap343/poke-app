bf15eaf092ed331625946b25fc5e202d69a9f467
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-07-13 13:52:13 -0700

    fix(pokemon-evolutions): add key property when mapping, change evolutions to evolve on line 30, and reduce return of the component

--- pokemon-app/src/components/PokemonEvolutions.js
+++ pokemon-app/src/components/PokemonEvolutions.js
@@ -1,46 +1,45 @@
 import React from "react";
 
-const PokemonEvolutions = ({ evolutions }) => {
-  return (
-    <div className="pokemon-evolutions">
-      <div className="pokemon-evolutions__title">
-        <p>Evolutions</p>
-      </div>
-
-      <div className="pokemon-evolutions__card-wrapper">
-        {!evolutions.length
-          ? null
-          : evolutions.map((evolve) => (
-              <div className="pokemon-evolutions__card">
-                <div className="pokemon-evolutions__img">
-                  <img
-                    src={`${evolve?.sprites?.front_default}`}
-                    alt={`${evolve?.name}`}
-                  />
-                </div>
-
-                <div className="pokemon-evolutions__name-id">
-                  <span className="pokemon-evolutions__name">
-                    {evolve?.name}
-                  </span>
-
-                  <span className="pokemon-evolutions__id">{evolve?.id}</span>
-                </div>
-
-                <div className="pokemon-evolutions__type">
-                  {!evolve?.types.length
-                    ? null
-                    : evolutions?.types.map((type) => (
-                        <span className="pokemon-evolutions__type-name">
-                          {type?.type?.name}
-                        </span>
-                      ))}
-                </div>
-              </div>
-            ))}
-      </div>
+const PokemonEvolutions = ({ evolutions }) => (
+  <div className="pokemon-evolutions">
+    <div className="pokemon-evolutions__title">
+      <p>Evolutions</p>
     </div>
-  );
-};
+
+    <div className="pokemon-evolutions__card-wrapper">
+      {!evolutions.length
+        ? null
+        : evolutions.map((evolve) => (
+            <div key={evolve?.id} className="pokemon-evolutions__card">
+              <div className="pokemon-evolutions__img">
+                <img
+                  src={`${evolve?.sprites?.front_default}`}
+                  alt={`${evolve?.name}`}
+                />
+              </div>
+
+              <div className="pokemon-evolutions__name-id">
+                <span className="pokemon-evolutions__name">{evolve?.name}</span>
+
+                <span className="pokemon-evolutions__id">{evolve?.id}</span>
+              </div>
+
+              <div className="pokemon-evolutions__type">
+                {!evolve?.types.length
+                  ? null
+                  : evolve?.types.map((type, typeIndex) => (
+                      <span
+                        key={`${evolve?.id}-${typeIndex}`}
+                        className="pokemon-evolutions__type-name"
+                      >
+                        {type?.type?.name}
+                      </span>
+                    ))}
+              </div>
+            </div>
+          ))}
+    </div>
+  </div>
+);
 
 export default PokemonEvolutions;
