fef9477a3ba702299d21181a77494526178d4798
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-07-12 21:48:41 -0700

    feat(movie-app-api): add gitignore to .env file for api url, create returnEvolutionChainData function which returns the necessary evolution data, create a refactor fetch async/await function for api calls, and create getPokeByName api function which return the necessary api pokemon data

--- pokemon-app/src/App.js
+++ pokemon-app/src/App.js
@@ -1,14 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Luis is the best
-        </p>
+        <p>Luis is the best</p>
         <a
           className="App-link"
           href="https://reactjs.org"
