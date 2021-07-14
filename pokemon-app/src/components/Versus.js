da675ee24bc5947340eba268f7327a80b8a89540
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-07-14 15:15:10 -0700

    feat(vs-animation): create a base layout of the component for the versus animation

---
+++ pokemon-app/src/components/Versus.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Versus = () => {
+  return (
+    <div className="pokemon-versus">
+      <div className="pokemon-versus__left">
+        <span className="pokemon-versus__left-main">V</span>
+      </div>
+
+      <div className="pokemon-versus__right">
+        <span className="pokemon-versus__right-main">S</span>
+      </div>
+    </div>
+  );
+};
+
+export default Versus;
