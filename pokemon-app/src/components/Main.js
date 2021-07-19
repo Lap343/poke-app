e02223de36cdbd665053532a2728c320ef17233e
lap343 <lap343@gmail.com>
2021-07-19 09:58:27 -0700

    committing before pull from origin

--- pokemon-app/src/components/Main.js
+++ pokemon-app/src/components/Main.js
@@ -1,12 +1,18 @@
-import splashBackground from "../assets/splash-background.png"
-import splashStar from "../assets/splash-star.png"
-import splashTitle from "../assets/splash-title.png"
+import { useState } from "react"
+import { getPokeByNameOrId } from "../utils/api"
+// import splashBackground from "../assets/splash-background.png"
+// import splashStar from "../assets/splash-star.png"
+// import splashTitle from "../assets/splash-title.png"
+import TwoSearched from "./TwoSearched"
 
 const Main = () => {
+    let pokemon = getPokeByNameOrId("ditto");
+
     return <div className="main">
-        <img className="splash-background" src={splashBackground} alt=""/>
+        {/* <img className="splash-background" src={splashBackground} alt=""/>
         <img className="splash-star" src={splashStar} alt=""/>
-        <img className="splash-title" src={splashTitle} alt=""/>
+        <img className="splash-title" src={splashTitle} alt=""/> */}
+        <TwoSearched name={pokemon}/>
     </div>
 }
 
---
+++ pokemon-app/src/components/TwoSearched.js
@@ -0,0 +1,5 @@
+const TwoSearched = ({ pokemon }) => {
+    return <div>{console.log(pokemon.name)}</div>
+}
+
+export default TwoSearched
