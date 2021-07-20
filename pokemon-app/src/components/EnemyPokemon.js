5cced7c0fc145f83843f0c31b7869e6d85c970c2
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-07-20 07:05:44 -0700

    feat(two-search-pokemon): add a lilly pad image

--- pokemon-app/src/components/EnemyPokemon.js
+++ pokemon-app/src/components/EnemyPokemon.js
@@ -1,33 +1,40 @@
 import React from "react";
+import fightPad from "../assets/fightpad.png";
 
 const EnemyPokemon = ({ enemyPokemon }) =>
   !enemyPokemon ? null : (
-    <div className="enemy-pokemon">
-      <div className="enemy-pokemon__display">
-        <div className="enemy-pokemon__display-name">
-          <p className="paragraph">{enemyPokemon?.name}</p>
+    <>
+      <div className="enemy-pokemon">
+        <div className="enemy-pokemon__display">
+          <div className="enemy-pokemon__display-name">
+            <p className="paragraph">{enemyPokemon?.name}</p>
+          </div>
+
+          <div className="enemy-pokemon__display-image">
+            <img
+              src={`${enemyPokemon?.sprites?.front_default}`}
+              alt={`Front default of ${enemyPokemon?.name} sprite`}
+            />
+          </div>
         </div>
 
-        <div className="enemy-pokemon__display-image">
-          <img
-            src={`${enemyPokemon?.sprites?.front_default}`}
-            alt={`Front default of ${enemyPokemon?.name} sprite`}
-          />
+        <div className="enemy-pokemon__stats">
+          <div className="enemy-pokemon__stats-title">Enemy moves:</div>
+
+          <ol className="enemy-pokemon__stats-list">
+            {!enemyPokemon?.moves
+              ? null
+              : enemyPokemon?.moves.map((moveData, moveIndex) => (
+                  <li key={moveIndex}>{moveData.move.name}</li>
+                ))}
+          </ol>
         </div>
       </div>
 
-      <div className="enemy-pokemon__stats">
-        <div className="enemy-pokemon__stats-title">Enemy moves:</div>
-
-        <ol className="enemy-pokemon__stats-list">
-          {!enemyPokemon?.moves
-            ? null
-            : enemyPokemon?.moves.map((moveData, moveIndex) => (
-                <li key={moveIndex}>{moveData.move.name}</li>
-              ))}
-        </ol>
+      <div className="poke-pad--enemy">
+        <img src={fightPad} alt="Lily pad from Pokemon game" />
       </div>
-    </div>
+    </>
   );
 
 export default EnemyPokemon;
