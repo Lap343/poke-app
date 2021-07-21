dd980729f529038a1b41be282653655e92ec4039
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-07-21 15:52:15 -0700

    feat(pokeball): create the Pokeball component

---
+++ pokemon-app/src/components/Pokeball.js
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Pokeball = () => (
+  <div className="pokeball-container">
+    <div className="pokeball">
+      <div className="pokeball__upper"></div>
+
+      <div className="pokeball__lower"></div>
+
+      <div className="pokeball__circle-button"></div>
+
+      <div className="pokeball__equator"></div>
+
+      <div className="pokeball__inner"></div>
+
+      <div className="pokeball__lower-equator"></div>
+    </div>
+
+    <div className="pokeball-whiteout"></div>
+  </div>
+);
+
+export default Pokeball;
