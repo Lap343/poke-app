3f12f543327654247c69a083e70ef65a007c8b98
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-07-22 15:48:25 -0700

    refactor(pokeball): reduce conditional rendering, and remove setIsPokeball from useEffect dependency

--- pokemon-app/src/components/Pokeball.js
+++ pokemon-app/src/components/Pokeball.js
@@ -7,26 +7,28 @@ const Pokeball = ({ isPokeball, setIsPokeball }) => {
       const timer = setTimeout(() => setIsPokeball(false), 1700);
       return () => clearTimeout(timer);
     }
-  }, [isPokeball, setIsPokeball]);
+  }, [isPokeball]);
 
-  return !isPokeball ? null : (
-    <div className="pokeball-container">
-      <div className="pokeball">
-        <div className="pokeball__upper"></div>
+  return (
+    isPokeball && (
+      <div className="pokeball-container">
+        <div className="pokeball">
+          <div className="pokeball__upper"></div>
 
-        <div className="pokeball__lower"></div>
+          <div className="pokeball__lower"></div>
 
-        <div className="pokeball__circle-button"></div>
+          <div className="pokeball__circle-button"></div>
 
-        <div className="pokeball__equator"></div>
+          <div className="pokeball__equator"></div>
 
-        <div className="pokeball__inner"></div>
+          <div className="pokeball__inner"></div>
 
-        <div className="pokeball__lower-equator"></div>
+          <div className="pokeball__lower-equator"></div>
+        </div>
+
+        <div className="pokeball-whiteout"></div>
       </div>
-
-      <div className="pokeball-whiteout"></div>
-    </div>
+    )
   );
 };
 
