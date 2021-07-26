988a9d8dc72399706008bc2afa38190f12143e08
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-07-26 14:25:29 -0700

    feat(pokemon-cry): add a reactoad.mp3 sound, and render the reactoad.mp3 in useEffect if that pokemon's name is present in PokemonCry.js

--- pokemon-app/src/components/PokemonCry.js
+++ pokemon-app/src/components/PokemonCry.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import pokeCries from "../utils/pokeCries";
+import reactoadSound from "../assets/sounds/reactoad.mp3";
 
 const PokemonCry = ({ pokeName }) => {
   const [isPokemonsCries, setIsPokemonsCries] = useState(false);
@@ -7,7 +8,10 @@ const PokemonCry = ({ pokeName }) => {
 
   // useEffect when a pokemon's name is present
   useEffect(() => {
-    if (pokeCries[pokeName]) {
+    if (pokeName === "Reactoad") {
+      setIsPokemonsCries(true);
+      setPokeSoundUrl(reactoadSound);
+    } else if (pokeCries[pokeName]) {
       setIsPokemonsCries(true);
       setPokeSoundUrl(pokeCries[pokeName]);
     }
