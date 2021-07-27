8cbaf4378cd39245374cb7be96bd8254d1f031c8
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-07-27 13:24:30 -0700

    feat(pokemon-cry): create a useRef call audioRef, reduce the first useEffect, expand the second useEffect to change the audioRef, increase the setTimeout length in the second useEffect, remove the autoPlay attribute in audio element, add audioRef as ref in audio element, and add comments for clarity

--- pokemon-app/src/components/PokemonCry.js
+++ pokemon-app/src/components/PokemonCry.js
@@ -1,36 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import pokeCries from "../utils/pokeCries";
 import reactoadSound from "../assets/sounds/reactoad.mp3";
 
 const PokemonCry = ({ pokeName }) => {
+  // Set the initial state for rendering the false.
   const [isPokemonsCries, setIsPokemonsCries] = useState(false);
+  // Set the sound url to an empty string
   const [pokeSoundUrl, setPokeSoundUrl] = useState("");
+  // Initialize the audio reference.
+  const audioRef = useRef();
 
-  // useEffect when a pokemon's name is present
+  // First useEffect sets the rendering to true, and sets the sound url
+  // which depends on the pokemon's name.
   useEffect(() => {
-    if (pokeName === "Reactoad") {
+    // If either names exist, continue.
+    if (pokeName === "Reactoad" || pokeCries[pokeName]) {
+      // Set the sound url path depending on the pokemon's name.
+      const pokeSoundUrlPath =
+        pokeName === "Reactoad" ? reactoadSound : pokeCries[pokeName];
+
+      // Set the rendering to true.
       setIsPokemonsCries(true);
-      setPokeSoundUrl(reactoadSound);
-    } else if (pokeCries[pokeName]) {
-      setIsPokemonsCries(true);
-      setPokeSoundUrl(pokeCries[pokeName]);
+      // Set the sound url path.
+      setPokeSoundUrl(pokeSoundUrlPath);
     }
   }, [pokeName]);
 
-  // useEffect for not displaying the audio
+  // Second useEffect changes the audio reference, and resets the rendering
+  // which depends on the rendering boolean state.
   useEffect(() => {
+    // If the rendering boolean state is true, proceed.
     if (isPokemonsCries) {
-      const timer = setTimeout(() => setIsPokemonsCries(false), 2000);
+      // This checks if the current audio reference exist,
+      // and if it does then continue.
+      if (audioRef.current) {
+        // Decrease the audio volume.
+        audioRef.current.volume = 0.1;
+        // Play the audio.
+        audioRef.current.play();
+      }
 
+      // Whether or not the current audio reference exists,
+      // remove the rendered component so it won't just "stay" there.
+      const timer = setTimeout(() => setIsPokemonsCries(false), 3000);
       return () => clearTimeout(timer);
     }
   }, [isPokemonsCries]);
 
   return (
     isPokemonsCries && (
-      <audio autoPlay className="pokemon-cry">
-        <source type="audio/mp3" src={pokeSoundUrl} />
-      </audio>
+      <audio ref={audioRef} className="pokemon-cry" src={pokeSoundUrl} />
     )
   );
 };
