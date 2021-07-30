dd8e3d18631a79e4a45598c87958d0a13686781c
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-07-30 15:59:33 -0700

    fix(fix-types-capitalize): create a capitalize function in capitalize.js, and implement capitalize function

--- pokemon-app/src/components/Types.js
+++ pokemon-app/src/components/Types.js
@@ -1,18 +1,19 @@
-import "../styles/Types.css"
+import "../styles/Types.css";
+import { capitalize } from "../utils";
 
 const Types = ({ pokemon }) => {
-    return (
-        <>
-            {pokemon?.types.map((type, typeIndex) => (
-                <span
-                key={`${pokemon?.id}-${typeIndex}`}
-                className={`type-name ${type?.type?.name}`}
-                >
-                {type?.type?.name}
-                </span>
-            ))}
-        </>
-    )
-}
+  return (
+    <>
+      {pokemon?.types.map((type, typeIndex) => (
+        <span
+          key={`${pokemon?.id}-${typeIndex}`}
+          className={`type-name ${type?.type?.name}`}
+        >
+          {capitalize(type?.type?.name)}
+        </span>
+      ))}
+    </>
+  );
+};
 
 export default Types;
