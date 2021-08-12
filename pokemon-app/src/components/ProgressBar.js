4c0c94e383b9af8562f6bedc261607f60dd7ad86
Elias Gutierrez <gutierrezelias1991@gmail.com>
2021-08-12 12:35:10 -0700

    feat(moves-game): style the ProgressBar component, and implement both clearTimeout and useEffect

--- pokemon-app/src/components/ProgressBar.js
+++ pokemon-app/src/components/ProgressBar.js
@@ -1,16 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import "../styles/ProgressBar.css";
 
 const ProgressBar = ({ done }) => {
   const [style, setStyle] = useState({});
 
-  setTimeout(() => {
-    const newStyle = {
-      opacity: 1,
-      width: `${done}%`,
-    };
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      const newStyle = {
+        opacity: 1,
+        width: `${done}%`,
+      };
 
-    setStyle(newStyle);
-  }, 200);
+      setStyle(newStyle);
+    }, 20);
+
+    return () => clearTimeout(timer);
+  }, [done]);
 
   return (
     <div className="progress-bar">
